c473e025540647d64d617d424e1c1dbff4fdfcda
Wendell Mendes <wendell.mendes@sti.ufc.br>
2021-07-19 11:35:54 -0300

    Logic as Control Flow Finder fixed

--- src/main/java/br/ufc/mdcc/bohr/finder/LogicAsControlFlowFinder.java
+++ src/main/java/br/ufc/mdcc/bohr/finder/LogicAsControlFlowFinder.java
@@ -1,5 +1,7 @@
 package br.ufc.mdcc.bohr.finder;
 
+import java.util.List;
+
 import br.ufc.mdcc.bohr.model.AoC;
 import br.ufc.mdcc.bohr.model.AoCInfo;
 import br.ufc.mdcc.bohr.model.Dataset;
@@ -8,6 +10,7 @@ import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.BinaryOperatorKind;
 import spoon.reflect.code.CtBinaryOperator;
 import spoon.reflect.code.CtExpression;
+import spoon.reflect.code.CtUnaryOperator;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.visitor.filter.TypeFilter;
 
@@ -16,22 +19,61 @@ public class LogicAsControlFlowFinder extends AbstractProcessor<CtClass<?>> {
 	public void process(CtClass<?> element) {
 		if (Util.isValid(element)) {
 			String qualifiedName = element.getQualifiedName();
-
+			
 			TypeFilter<CtBinaryOperator<?>> binaryOperatorFilter = new TypeFilter<CtBinaryOperator<?>>(CtBinaryOperator.class);
+			TypeFilter<CtUnaryOperator<?>> unaryOperatorFilter = new TypeFilter<CtUnaryOperator<?>>(CtUnaryOperator.class);
 
 			for (CtBinaryOperator<?> operator : element.getElements(binaryOperatorFilter)) {
 				
 				if(operator.getKind() == BinaryOperatorKind.AND || operator.getKind() == BinaryOperatorKind.OR) {
 					
 					CtExpression<?> leftHandOperand = operator.getLeftHandOperand();
-					if(!leftHandOperand.getElements(binaryOperatorFilter).isEmpty()) {
-						int lineNumber = operator.getPosition().getEndLine();
-						String snippet = operator.getParent().prettyprint();
+					CtExpression<?> rightHandOperand = operator.getRightHandOperand();
+					
+					List<CtUnaryOperator<?>> leftHandUnaryOperators = leftHandOperand.getElements(unaryOperatorFilter);
+					List<CtUnaryOperator<?>> rightHandUnaryOperators = rightHandOperand.getElements(unaryOperatorFilter);
+					
+					if(!leftHandUnaryOperators.isEmpty() || !rightHandUnaryOperators.isEmpty()) {
 						
-						Dataset.store(qualifiedName, new AoCInfo(AoC.LaCTRF, lineNumber, snippet));
+						if(hasUnaryOperators(leftHandUnaryOperators) || hasUnaryOperators(rightHandUnaryOperators)) {
+							int lineNumber = operator.getPosition().getEndLine();
+							String snippet = operator.getParent().prettyprint();
+							
+							Dataset.store(qualifiedName, new AoCInfo(AoC.LaCTRF, lineNumber, snippet));
+						}
+						
 					}
 				}
 			}
 		}
 	}
+	
+	private boolean hasUnaryOperators(List<CtUnaryOperator<?>> unaryOperators) {
+		
+		for (CtUnaryOperator<?> operator : unaryOperators) {
+
+			switch (operator.getKind()) {
+				case PREINC:
+					return true;
+					
+				case PREDEC:
+					return true;
+					
+				case POS:
+					return true;
+					
+				case POSTINC:
+					return true;
+					
+				case POSTDEC:
+					return true;
+					
+				default:
+					break;
+				}
+		}
+
+		return false;
+	}
+	
 }
