ac25ae746bb846b62125a9935c204c951be5de99
Wendell Mendes <wendell.mendes@sti.ufc.br>
2021-07-21 15:36:54 -0300

    Conditional Operator Fix

--- src/main/java/br/ufc/mdcc/bohr/finder/ConditionalOperatorFinder.java
+++ src/main/java/br/ufc/mdcc/bohr/finder/ConditionalOperatorFinder.java
@@ -5,7 +5,9 @@ import br.ufc.mdcc.bohr.model.AoCInfo;
 import br.ufc.mdcc.bohr.model.Dataset;
 import br.ufc.mdcc.bohr.util.Util;
 import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.CtAssignment;
 import spoon.reflect.code.CtConditional;
+import spoon.reflect.code.CtLocalVariable;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.visitor.filter.TypeFilter;
 
@@ -17,9 +19,12 @@ public class ConditionalOperatorFinder extends AbstractProcessor<CtClass<?>> {
 
 			TypeFilter<CtConditional<?>> filter = new TypeFilter<CtConditional<?>>(CtConditional.class);
 			for (CtConditional<?> condOpr : element.getElements(filter)) {
-				int lineNumber = condOpr.getPosition().getEndLine();
-				String snippet = condOpr.getParent().prettyprint();
-				Dataset.store(qualifiedName, new AoCInfo(AoC.CoO, lineNumber, snippet));
+				if ((condOpr.getParent() != null) && ((condOpr.getParent() instanceof CtAssignment)
+						|| condOpr.getParent() instanceof CtLocalVariable)) {
+					int lineNumber = condOpr.getPosition().getEndLine();
+					String snippet = condOpr.getParent().prettyprint();
+					Dataset.store(qualifiedName, new AoCInfo(AoC.CoO, lineNumber, snippet));
+				}
 			}
 		}
 	}
