875fa4c8ff774b03e33386985b69d5dcdc097928
Wendell Mendes <wendell.mendes@sti.ufc.br>
2021-10-27 20:11:00 -0300

    Repurposed Variables Finder improvements

--- src/main/java/br/ufc/mdcc/bohr/finder/RepurposedVariablesFinder.java
+++ src/main/java/br/ufc/mdcc/bohr/finder/RepurposedVariablesFinder.java
@@ -1,12 +1,19 @@
 package br.ufc.mdcc.bohr.finder;
 
+import java.util.List;
+
 import br.ufc.mdcc.bohr.model.AoC;
 import br.ufc.mdcc.bohr.model.AoCInfo;
 import br.ufc.mdcc.bohr.model.Dataset;
 import br.ufc.mdcc.bohr.util.Util;
 import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.CtArrayRead;
+import spoon.reflect.code.CtArrayWrite;
+import spoon.reflect.code.CtExpression;
 import spoon.reflect.code.CtFor;
+import spoon.reflect.code.CtLoop;
 import spoon.reflect.code.CtStatement;
+import spoon.reflect.code.CtWhile;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.visitor.filter.TypeFilter;
 import spoon.support.reflect.code.CtAssignmentImpl;
@@ -20,51 +27,96 @@ public class RepurposedVariablesFinder extends AbstractProcessor<CtClass<?>> {
 		if (Util.isValid(element)) {
 			String qualifiedName = element.getQualifiedName();
 
-			TypeFilter<CtFor> filter = new TypeFilter<CtFor>(CtFor.class);
-			for (CtFor forLoop : element.getElements(filter)) {
+			getLoopCase(element, qualifiedName);
+
+			getForUpdateCase(element, qualifiedName);
+		}
+	}
+
+	private void getForUpdateCase(CtClass<?> element, String qualifiedName) {
+		TypeFilter<CtFor> filter = new TypeFilter<CtFor>(CtFor.class);
+		for (CtFor forLoop : element.getElements(filter)) {
+			
+			if(forLoop.getParent().getParent() instanceof CtFor) {
 				
-				if(forLoop.getParent().getParent() instanceof CtFor) {
+				CtFor forParent = (CtFor) forLoop.getParent().getParent();
+				
+				if(!forParent.getForInit().isEmpty() && !forLoop.getForUpdate().isEmpty()) {
 					
-					CtFor forParent = (CtFor) forLoop.getParent().getParent();
+					CtStatement forParentInit = forParent.getForInit().get(0);
+					CtStatement forLoopUpdate = forLoop.getForUpdate().get(0);
 					
-					if(!forParent.getForInit().isEmpty() && !forLoop.getForUpdate().isEmpty()) {
+					String forParentInitVariableString = "";
+					if(forParentInit instanceof CtLocalVariableImpl) {
+						CtLocalVariableImpl<?> forParentInitVariable = (CtLocalVariableImpl<?>) forParentInit;
+						forParentInitVariableString = forParentInitVariable.getSimpleName();
+					} else if(forParentInit instanceof CtAssignmentImpl){
+						CtAssignmentImpl<?, ?> forParentInitAssignment = (CtAssignmentImpl<?, ?>) forParentInit;
+						forParentInitVariableString = forParentInitAssignment.getAssigned().prettyprint();
+					}
+					
+					if(forLoopUpdate instanceof CtUnaryOperatorImpl) {
+						CtUnaryOperatorImpl<?> forLoopUpdateVariable = (CtUnaryOperatorImpl<?>) forLoopUpdate;
 						
-						CtStatement forParentInit = forParent.getForInit().get(0);
-						CtStatement forLoopUpdate = forLoop.getForUpdate().get(0);
-						
-						String forParentInitVariableString = "";
-						if(forParentInit instanceof CtLocalVariableImpl) {
-							CtLocalVariableImpl<?> forParentInitVariable = (CtLocalVariableImpl<?>) forParentInit;
-							forParentInitVariableString = forParentInitVariable.getSimpleName();
-						} else if(forParentInit instanceof CtAssignmentImpl){
-							CtAssignmentImpl<?, ?> forParentInitAssignment = (CtAssignmentImpl<?, ?>) forParentInit;
-							forParentInitVariableString = forParentInitAssignment.getAssigned().prettyprint();
+						if(forParentInitVariableString.equals(forLoopUpdateVariable.getOperand().prettyprint())) {
+							store(qualifiedName, forParent);
 						}
 						
-						if(forLoopUpdate instanceof CtUnaryOperatorImpl) {
-							CtUnaryOperatorImpl<?> forLoopUpdateVariable = (CtUnaryOperatorImpl<?>) forLoopUpdate;
-							
-							if(forParentInitVariableString.equals(forLoopUpdateVariable.getOperand().prettyprint())) {
-								store(qualifiedName, forParent);
-							}
-							
-						} else if(forLoopUpdate instanceof CtOperatorAssignmentImpl<?, ?>) {
-							CtOperatorAssignmentImpl<?, ?> forLoopUpdateVariable = (CtOperatorAssignmentImpl<?, ?>) forLoopUpdate;
-							
-							if(forParentInitVariableString.equals(forLoopUpdateVariable.getAssigned().prettyprint())) {
-								store(qualifiedName, forParent);
+					} else if(forLoopUpdate instanceof CtOperatorAssignmentImpl<?, ?>) {
+						CtOperatorAssignmentImpl<?, ?> forLoopUpdateVariable = (CtOperatorAssignmentImpl<?, ?>) forLoopUpdate;
+						
+						if(forParentInitVariableString.equals(forLoopUpdateVariable.getAssigned().prettyprint())) {
+							store(qualifiedName, forParent);
+						}
+					}
+					
+				}
+			}
+		}
+	}
+	
+	private void getLoopCase(CtClass<?> element, String qualifiedName) {
+		TypeFilter<CtLoop> whileFilter = new TypeFilter<CtLoop>(CtLoop.class);
+		TypeFilter<CtArrayRead<?>> arrayReadFilter = new TypeFilter<CtArrayRead<?>>(CtArrayRead.class);
+		TypeFilter<CtArrayWrite<?>> arrayWriteFilter = new TypeFilter<CtArrayWrite<?>>(CtArrayWrite.class);
+		
+		for (CtLoop ctLoop : element.getElements(whileFilter)) {
+			
+			CtExpression<?> loopingExpression = null;
+			if(ctLoop instanceof CtFor) {
+				loopingExpression = ((CtFor) ctLoop).getExpression();
+			}else if(ctLoop instanceof CtWhile) {
+				loopingExpression = ((CtWhile) ctLoop).getLoopingExpression();
+			}
+			
+			if(loopingExpression != null) {
+				List<CtArrayRead<?>> arraysRead = loopingExpression.getElements(arrayReadFilter);
+				
+				if(!arraysRead.isEmpty()) {
+					for (CtArrayRead<?> read : arraysRead) {
+						CtStatement body = ctLoop.getBody();
+						
+						if(body != null) {
+							List<CtArrayWrite<?>> arraysWrite = body.getElements(arrayWriteFilter);
+							if(!arraysWrite.isEmpty()) {
+								for (CtArrayWrite<?> write : arraysWrite) {
+									if(write.getTarget().prettyprint().equalsIgnoreCase(read.getTarget().prettyprint())) {
+										store(qualifiedName, ctLoop);
+										break;
+									}
+								}
 							}
 						}
-						
 					}
 				}
 			}
 		}
 	}
 
-	private void store(String qualifiedName, CtFor forParent) {
-		int lineNumber = forParent.getPosition().getLine();
-		String snippet = forParent.prettyprint();
+	private void store(String qualifiedName, CtLoop loop) {
+		int lineNumber = loop.getPosition().getLine();
+		String snippet = loop.prettyprint();
 		Dataset.store(qualifiedName, new AoCInfo(AoC.RVar, lineNumber, snippet));
 	}
+	
 }
--- src/test/resources/RepurposedVariables/RepurposedVariablesSample.java
+++ src/test/resources/RepurposedVariables/RepurposedVariablesSample.java
@@ -51,4 +51,72 @@ public class RepurposedVariablesSample {
 		
 		System.out.println(a);
 	}
+	
+	private void thirdMethod() {
+		int v1[] = new int[5];
+		v1[4] = 3;
+		
+		while (v1[4] > 0) {
+			v1[3 - v1[4]] = v1[4];
+			v1[4] = v1[4] - 1;
+		}
+		System.out.println(v1[1] + " " + v1[4]);
+	}
+	
+	private void thirdMethodTransformed() {
+		int v1[] = new int[5];
+		int a = 5;
+		
+		while (a > 0) {
+			v1[3 - v1[4]] = v1[4];
+			a = a - 1;
+		}
+		System.out.println(v1[1] + " " + v1[4]);
+	}
+	
+	private void firstVariationMethod() {
+		int v1[] = new int[5];
+		int v2[] = new int[5];
+		v1[4] = 3;
+		
+		while (v1[4] > 0 || v2[4] > 0) {
+			v2[3 - v1[3]] = v1[2];
+			v2[3] = v1[4] - 1;
+		}
+		System.out.println(v1[1] + " " + v1[4]);
+	}
+	
+	private void secondVariationMethod() {
+		int v1[] = new int[5];
+		int v2[] = new int[5];
+		int a = 5;
+		int b = 5;
+		
+		while (a > 0 || b > 0) {
+			v2[3 - v1[3]] = v1[2];
+			a = a - 1;
+			b = b - 1;
+		}
+		System.out.println(v1[1] + " " + v1[4]);
+	}
+	
+	private void thirdVariationMethod() {
+		int v1[] = new int[5];
+		v1[4] = 3;
+		for (int i = 0; v1[i] > 0; i++) {
+			v1[3 - v1[4]] = v1[4];
+			v1[4] = v1[4] - 1;
+		}
+		System.out.println(v1[1] + " " + v1[4]);
+	}
+	
+	private void fourthMethod() {
+		int v1[] = new int[5];
+
+		for (int i = 0; i > 0; i++) {
+			v1[3 - v1[4]] = v1[4];
+			v1[4] = v1[4] - 1;
+		}
+		System.out.println(v1[1] + " " + v1[4]);
+	}
 }
